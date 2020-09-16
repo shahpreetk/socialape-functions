f3ad4a0c93d7d8d5e999c1126654848659d4c2e9
Preet Shah <shahpreetk@gmail.com>
2020-09-16 17:16:59 +0530

    creating a scream possible

--- functions/index.js
+++ functions/index.js
@@ -22,3 +22,25 @@ exports.getScreams = functions.https.onRequest((req,res) => {
     })
     .catch(err => console.error(err));
 })
+
+exports.createScream = functions.https.onRequest((req,res) => {
+  if(req.method !== 'POST') {
+    return res.status(400).json({error: 'Method not allowed'});
+  }
+  const newScream = {
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+  };
+
+  admin.firestore()
+    .collection('screams')
+    .add(newScream)
+    .then(doc => {
+      res.json({message: `document ${doc.id} created successfully`})
+    })
+    .catch(err => {
+      res.status(500).json({error: 'something went wrong'});
+      console.error(err);
+    })
+})
