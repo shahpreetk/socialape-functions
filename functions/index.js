450df4484a1201a905bff2a099e145406088d515
Preet Shah <shahpreetk@gmail.com>
2020-09-16 16:57:15 +0530

    gets screams

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,7 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,3 +10,15 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello worlds!");
 });
+
+exports.getScreams = functions.https.onRequest((req,res) => {
+  admin.firestore().collection('screams').get()
+    .then(data => {
+      let screams = [];
+      data.forEach(doc => {
+        screams.push(doc.data());
+      });
+      return res.json(screams);
+    })
+    .catch(err => console.error(err));
+})
