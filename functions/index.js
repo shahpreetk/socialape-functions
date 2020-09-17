44ef57274c110896d8ffcee92068bf60e0e27d4c
Preet Shah <shahpreetk@gmail.com>
2020-09-17 22:49:45 +0530

    validation issue removed

--- functions/index.js
+++ functions/index.js
@@ -65,14 +65,14 @@ app.post('/scream', (req, res) => {
 })
 
 // Function Checking if email is valid or not
-const isEmail=(email)=>{
+const isEmail = (email) => {
   const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if (email.match(regEx)) return true;
   else return false;
 }
 // Function Checking if string is empty or not
 const isEmpty = (string) => {
-  if(string.trim() === '') return true;
+  if (string.trim() === '') return true;
   else return false;
 }
 
@@ -81,23 +81,23 @@ app.post('/signup', (req, res) => {
   const newUser = {
     email: req.body.email,
     password: req.body.password,
-    confirmPassword: req.body.email,
+    confirmPassword: req.body.confirmPassword,
     userHandle: req.body.userHandle
   }
 
-  let errors={};
+  let errors = {};
 
-  if(isEmpty(newUser.email)){
+  if (isEmpty(newUser.email)) {
     errors.email = 'Please fill it in'
-  } else if (!isEmail(newUser.email)){
+  } else if (!isEmail(newUser.email)) {
     errors.email = 'Please fill a valid Email address'
   }
 
-  if(isEmpty(newUser.password)) errors.password = 'Please fill it in'
-  if(newUser.password !== newUser.confirmPassword) errors.confirmPassword = 'Passwords must be the same'
-  if(isEmpty(newUser.userHandle)) errors.userHandle = 'Please fill it in'
+  if (isEmpty(newUser.password)) errors.password = 'Please fill it in';
+  if (newUser.password !== newUser.confirmPassword) errors.confirmPassword = 'Passwords must be the same';
+  if (isEmpty(newUser.userHandle)) errors.userHandle = 'Please fill it in';
 
-  if(Object.keys(errors).length > 0) return res.status(400).json(errors);
+  if (Object.keys(errors).length > 0) return res.status(400).json(errors);
 
   let token, userId;
   db.doc(`/users/${newUser.userHandle}`)
