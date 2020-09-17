e240631459175bc91b4ebcb34e096973b1cd1ee8
Preet Shah <shahpreetk@gmail.com>
2020-09-17 22:28:28 +0530

    email validation added

--- functions/index.js
+++ functions/index.js
@@ -64,6 +64,12 @@ app.post('/scream', (req, res) => {
     })
 })
 
+// Function Checking if email is valid or not
+const isEmail=(email)=>{
+  const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (email.match(regEx)) return true;
+  else return false;
+}
 // Function Checking if string is empty or not
 const isEmpty = (string) => {
   if(string.trim() === '') return true;
@@ -77,6 +83,14 @@ app.post('/signup', (req, res) => {
     password: req.body.password,
     confirmPassword: req.body.email,
     userHandle: req.body.userHandle
+  }
+
+  let errors={};
+
+  if(isEmpty(newUser.email)){
+    errors.email = 'Please fill in an Email address'
+  } else if (!isEmail(newUser.email)){
+    errors.email = 'Please fill a valid Email address'
   }
 
   let token, userId;
