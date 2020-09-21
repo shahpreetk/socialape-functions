9ace3b04aab56e547c02c31c59597078c2d20443
Preet Shah <shahpreetk@gmail.com>
2020-09-21 15:56:11 +0530

    FBAuth function made

--- functions/index.js
+++ functions/index.js
@@ -39,12 +39,40 @@ app.get('/screams', (req, res) => {
     .catch(err => console.error(err));
 })
 
+const FBAuth = (req, res, next) => {
+  let idToken;
+  if(req.headers.authorization && req.headers.authorization.startsWith('Bearer ')){
+    idToken = req.headers.authorization.split('Bearer ')[1];
+  } else{
+    console.error('No token found');
+    return res.status(403).json({error: 'Unauthorized'});
+  }
+
+  admin.auth().verifyIdToken(idToken)
+    .then(decodedToken => {
+      req.user = decodedToken;
+      console.log(decodedToken);
+      return db.collection('users')
+        .where('userId', '==', req.user.uid)
+        .limit(1)
+        .get();
+    })
+    .then(data => {
+      req.user.userHandle = data.docs[0].data().userHandle;
+      return next();
+    })
+    .catch(err => {
+      console.error('Error while verifying token ', err);
+      return res.status(403).json(err);
+    })
+}
+
 // Creates a Scream
-app.post('/scream', (req, res) => {
+app.post('/scream', FBAuth, (req, res) => {
 
   const newScream = {
     body: req.body.body,
-    userHandle: req.body.userHandle,
+    userHandle: req.user.userHandle,
     createdAt: new Date().toISOString()
   };
 
