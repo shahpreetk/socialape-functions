7388b011803ecd03391ada1505100fad4b2be805
Preet Shah <shahpreetk@gmail.com>
2020-10-06 16:43:03 +0530

    comment count incememnts

--- functions/handlers/screams.js
+++ functions/handlers/screams.js
@@ -88,7 +88,10 @@ exports.commentOnScream = (req,res) => {
       if(!doc.exists){
         return res.status(404).json({error: 'Scream not found'});
       }
-      return db.collection('comments').add(newComment);
+      return doc.ref.update({commentCount: doc.data().commentCount+1});
+    })
+    .then(()=>{
+      return db.collection('comments').add(newComment)
     })
     .then(()=>{
       res.json(newComment);
