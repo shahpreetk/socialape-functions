2233a463bf736e043800fd5b2d842c7e0b084f9f
Preet Shah <shahpreetk@gmail.com>
2020-10-08 14:52:22 +0530

    made onUserImageChange

--- functions/index.js
+++ functions/index.js
@@ -61,7 +61,8 @@ exports.createNotificationOnLike = functions.region('europe-west1').firestore.do
             }
         })
         .catch(err => {
-            console.error(err))
+            console.error(err)
+        })
 })
 
 exports.deleteNotificationOnUnlike = functions.region('europe-west1').firestore.document('likes/{id}').onDelete(snapshot => {
@@ -91,3 +92,22 @@ exports.createNotificationOnComment = functions.region('europe-west1').firestore
             return;
         })
 })
+
+// change profile pic on all screams on update of profile pic
+exports.onUserImageChange = functions.region('europe-west1').firestore.document('/users/{userId}').onUpdate(change => {
+    console.log(change.before.data())
+    console.log(change.after.data())
+    if(change.before.data().imageUrl !== change.after.data().imageUrl){
+        console.log('image has changed')
+        let batch = db.batch()
+        return db.collection('screams').where('userHandle', '==', change.before.data().userHandle).get()
+        .then((data)=>{
+            data.forEach(doc => {
+                const scream = db.doc(`/screams/${doc.id}`)
+                batch.update(scream, {userImage: change.after.data().imageUrl})
+            })
+            return batch.commit()
+        })
+    }
+})
+
